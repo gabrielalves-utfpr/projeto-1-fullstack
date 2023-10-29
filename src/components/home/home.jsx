75d5d9a09fa7afb49405d37a8282e23b6b0ec2f1
gabrielalves-utfpr <gabrielalvessantos@alunos.utfpr.edu.br>
2023-10-29 15:22:30 -0300

    search

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,19 @@
 import './style.css'
+import { useState } from 'react';
 import Search from './search.jsx'
 
 export default function Home(){
+    const [data, setData] = useState(null);
+    const handleSearch = (response) => {
+        // Do something with the response here
+        setData(response)
+      }
     return(
         <div class = 'image-container'>
-            <Search/>
+            <Search onSearch={handleSearch}/>
             <div className="scrolldown">
                 <h3>Scroll Down</h3>
                 <h3>\↓/</h3>
-                
             </div>
         </div>
     )
--- src/components/home/search.jsx
+++ src/components/home/search.jsx
@@ -1,4 +1,17 @@
-export default function Search () { 
+import { useState } from 'react';
+import searchAPI from '../../nasaApi/searchApi.js';
+
+export default function Search ({ onSearch }) { 
+  const [text, setText] = useState('');
+
+  const handleKeyDown = async (event) => {
+    //const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const response = await searchAPI.search(text);
+      onSearch(response);
+      //onSearch(text);
+    }
+  }
   return (
     <div className="search">
       <h1 className="welcome">
@@ -10,6 +23,8 @@ export default function Search () {
         name="text"
         placeholder="Search..."
         className="input"
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <a href="/" id="search-bt"><img id="search-ico" src="/public/outline_search_white_24dp.png" alt="" /></a>
     </div>
