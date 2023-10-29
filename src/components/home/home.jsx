28d8ffc60e95d6b71a463dd2bcae99c975d54af4
gabrielalves-utfpr <gabrielalvessantos@alunos.utfpr.edu.br>
2023-10-29 00:13:18 -0300

    image of the day layout

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,6 +5,11 @@ export default function Home(){
     return(
         <div class = 'image-container'>
             <Search/>
+            <div className="scrolldown">
+                <h3>Scroll Down</h3>
+                <h3>\↓/</h3>
+                
+            </div>
         </div>
     )
 }
--- src/components/imageOfDay/iod.jsx
+++ src/components/imageOfDay/iod.jsx
@@ -1,7 +1,28 @@
+import'./style.css'
+
 export default function Iod(){
     return(
         <div class = 'image-container'>
-            <h1>Planets</h1>
+            
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>   
+            <div id="news">
+                
+                <div className="image-of-the-day">
+                    <div className="image-box">
+                        <h4 className="tag">IMAGE OF THE DAY</h4>
+                        <div className="overlay-text">
+                            <h1>Space Station Flight Over the Southern Tip of Italy</h1>
+                            <p>
+                                The southern tip of Italy is visible in this image taken by the
+                                Expedition 49 crew aboard the International Space Station on Sept.
+                                17, 2016. The brightly lit city of Naples can be seen in the bottom
+                                section of the image A Russian Soyuz spacecraft can be seen in the
+                                foreground,
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
